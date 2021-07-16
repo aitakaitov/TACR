632307c10eefd7f92f98ab9c0a6858b1d9ca8ed4
vojtech <aitakaitov@gmail.com>
2021-07-16 21:31:41 +0200

    Added AD crawlers

---
+++ ad_crawler/crawler_ctk.py
@@ -0,0 +1,142 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import JavascriptException
+
+from library_methods import LibraryMethods
+from log import Log
+from persistent_list import PersistentList
+
+from bs4 import BeautifulSoup
+import urllib.parse
+
+import os
+import traceback
+
+root_folder = "ad_pages"
+site_folder = "ctk"
+log_path = "log_ctk.log"
+chromedriver_path = "./chromedriver"
+to_visit_file = "TO_VISIT.PERSISTENT"
+visited_file = "VISITED.PERSISTENT"
+starting_page = "https://www.ceskenoviny.cz/pr/"
+max_scrolls = 1
+
+
+class Crawler:
+
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--incognito")
+
+        self.log = Log(log_path)
+
+        ''' Selenium driver for chrome'''
+        try:
+            self.driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+        except WebDriverException:
+            self.log.log("[CRAWLER] Chromedriver '" + chromedriver_path + "' not found, trying .exe")
+            try:
+                self.driver = webdriver.Chrome(executable_path=chromedriver_path + ".exe", options=chrome_options)
+            except WebDriverException:
+                self.log.log("[CRAWLER] No chromedriver found, exiting")
+                exit(1)
+
+        ''' Page load timeout'''
+        self.driver.set_page_load_timeout(20)
+
+        ''' List of links to visit '''
+        self.links_to_visit = PersistentList(to_visit_file)
+
+        try:
+            os.mkdir("./" + root_folder)
+        except OSError:
+            self.log.log("[CRAWLER] Pages directory already exists.")
+
+        try:
+            os.mkdir("./" + root_folder + "/" + site_folder)
+        except OSError:
+            pass
+
+    def start_crawler(self):
+        """
+        Starts the crawler from a starting url. The crawler will collect all usable links and then place then in a queue,
+        collecting more links as it goes.
+        :return:
+        """
+
+        # Test if we have no links from previous run
+        try:
+            self.collect_links(starting_page)
+            self.download_links()
+        except (WebDriverException, JavascriptException):
+            self.log.log("Error loading starting page, will exit.")
+            traceback.print_exc()
+            return
+
+    def collect_links(self, page):
+        self.log.log("Collecting links")
+        html = LibraryMethods.download_page_html(self.driver, page, max_scrolls)
+
+        soup = BeautifulSoup(html)
+        li_tags = soup.find_all("li", {"class": "list-item"}, recursive=True)
+
+        for tag in li_tags:
+            a_tag = tag.find("a", recursive=True)
+
+            if a_tag is None:
+                continue
+
+            tag_url = a_tag.get("href")
+            if urllib.parse.urljoin(page, tag_url) not in self.links_to_visit:
+                self.links_to_visit.append(urllib.parse.urljoin(page, tag_url))
+
+    def download_links(self):
+        self.log.log("Downloading pages")
+        html_folder = root_folder + "/" + site_folder + "/html"
+        plaintext_folder = root_folder + "/" + site_folder + "/plaintext"
+        p_folder = root_folder + "/" + site_folder + "/plaintext_with_p"
+
+        try:
+            os.mkdir(html_folder)
+            os.mkdir(plaintext_folder)
+            os.mkdir(p_folder)
+        except FileExistsError:
+            pass
+
+        for url in self.links_to_visit:
+            self.log.log("Processing " + url)
+
+            try:
+                html = LibraryMethods.download_page_html(self.driver, url, 20)
+            except WebDriverException:
+                continue
+
+            soup = BeautifulSoup(html)
+            LibraryMethods.filter_html(soup)
+            self.remove_article_heading(soup)
+
+            with open(html_folder + "/" + url.replace("/", "_"), "w+", encoding='utf-8') as f:
+                f.write(soup.prettify())
+
+            with open(plaintext_folder + "/" + url.replace("/", "_"), "w+", encoding='utf-8') as f:
+                f.write(BeautifulSoup(soup.prettify()).getText())
+
+            with open(p_folder + "/" + url.replace("/", "_"), "w+", encoding='utf-8') as f:
+                LibraryMethods.keep_paragraphs(soup)
+                f.write(soup.prettify())
+
+    def remove_article_heading(self, soup):
+        tag = soup.find("div", {"class": "box-article-info"})
+        if tag is not None:
+            tag.extract()
+
+
+Crawler().start_crawler()
+
+
+
+
+
+
