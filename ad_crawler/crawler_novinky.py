27b4f350524db094127fc268789b19585fc2db14
vojtech <aitakaitov@gmail.com>
2021-07-19 19:10:07 +0200

    Relevant text extraction for Aktualne OK

--- ad_crawler/crawler_novinky.py
+++ ad_crawler/crawler_novinky.py
@@ -152,7 +152,7 @@ class Crawler:
         article_tag = soup.find("article", {"class": "g_gl"})
         tags = article_tag.find_all()
 
-        valid_tags = ["div", "p", "h1", "h2", "h3", "h4", "h5"]
+        valid_tags = ["div", "p", "h1", "h2", "h3", "h4", "h5", "a"]
         for tag in tags:
             if tag.name == "p":
                 tag.attrs = {}
