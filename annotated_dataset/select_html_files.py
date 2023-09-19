df77d87403280feb4c7e5888cf72a69c0ca26daf
aitakaitov <aitakaitov@gmail.com>
2023-09-19 18:12:44 +0200

    improved annotated dataset processing

--- annotated_dataset/select_html_files.py
+++ annotated_dataset/select_html_files.py
@@ -85,6 +85,17 @@ def load_csv():
 
     data = []
     for index, row in df.iterrows():
+        if args['all_agree']:
+            if row['majority_fraction'] < 1:
+                print('rejecting due to majority fraction less than 1')
+                continue
+
+        frac_per_an = row['majority_fraction'] / row['majority_size']
+        annotators = row['majority_size'] + (1 - row['majority_fraction']) / frac_per_an
+        if annotators < args['min_annotators']:
+            print('rejecting due to annotators')
+            continue
+
         url = row['url']
         label = row['state']
         data.append({
@@ -157,12 +168,18 @@ def main():
     export(data_dict)
 
 
+def parse_bool(s):
+    return s.lower() == 'true'
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--fraction', type=str, required=True)
     parser.add_argument('--output_file', type=str, required=False, default='dataset_annotated.jsonl')
     parser.add_argument('--trim_text', default=None, required=False)
     parser.add_argument('--trim_length', default=0, required=False, type=int)
+    parser.add_argument('--min_annotators', default=1, type=int, required=False)
+    parser.add_argument('--all_agree', default=False, type=parse_bool, required=False)
     args = vars(parser.parse_args())
 
     main()
